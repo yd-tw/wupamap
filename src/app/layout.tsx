07dfa5df2b34fae8e69601ad2cb5d1296861d6ca
yd-tw <yd960528@gmail.com>
2025-02-02 14:00:46 +0800

    update to 1.0.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics gaId="G-J0NR2X4QTZ" />
         {children}
       </body>
     </html>
