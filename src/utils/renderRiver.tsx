fc184b31b2f1746845f4776dc44ee8d436fa48ba
yd-tw <yd960528@gmail.com>
2025-02-24 23:09:33 +0800

    add firebase config

--- src/utils/renderRiver.tsx
+++ src/utils/renderRiver.tsx
@@ -1,4 +1,6 @@
 import * as d3 from "d3";
+import { collection, getDocs } from "firebase/firestore";
+import db from "@/utils/firebase";
 
 interface River {
   id: string;
@@ -12,26 +14,48 @@ interface Point {
   y: number;
 }
 
-export function renderRiver(
+async function fetchRivers(): Promise<River[]> {
+  try {
+    const querySnapshot = await getDocs(collection(db, "rivers"));
+    const rivers: River[] = [];
+
+    querySnapshot.forEach((doc: { data: () => any; id: any; }) => {
+      const data = doc.data();
+      rivers.push({
+        id: doc.id,
+        name: data.name,
+        width: data.width,
+        points: data.points,
+      });
+    });
+
+    return rivers;
+  } catch (error) {
+    console.error("讀取 Firestore 數據時出錯:", error);
+    return [];
+  }
+}
+
+export async function renderRiver(
   g: d3.Selection<SVGGElement, unknown, null, undefined>,
 ) {
-  d3.json<River[]>("/river.json").then((data) => {
-    if (!data) return;
+  const rivers = await fetchRivers();
 
-    data.forEach((river) => {
-      g.append("path")
-        .datum(river.points)
-        .attr("fill", "none")
-        .attr("stroke", "LightBlue")
-        .attr("stroke-width", river.width)
-        .attr(
-          "d",
-          d3
-            .line<Point>()
-            .curve(d3.curveBasis)
-            .x((d) => d.x)
-            .y((d) => d.y),
-        );
-    });
+  if (rivers.length === 0) return;
+
+  rivers.forEach((river) => {
+    g.append("path")
+      .datum(river.points)
+      .attr("fill", "none")
+      .attr("stroke", "LightBlue")
+      .attr("stroke-width", river.width)
+      .attr(
+        "d",
+        d3
+          .line<Point>()
+          .curve(d3.curveBasis)
+          .x((d) => d.x)
+          .y((d) => d.y),
+      );
   });
 }
