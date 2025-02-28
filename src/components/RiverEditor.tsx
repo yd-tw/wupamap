638162cf8517f00e819ccdac3f37aa0ad820d748
yd-tw <yd960528@gmail.com>
2025-02-28 23:02:59 +0800

    add river edit func

--- src/components/RiverEditor.tsx
+++ src/components/RiverEditor.tsx
@@ -2,6 +2,8 @@ import { River } from "@/types/river";
 import { fetchCollection } from "@/utils/fetchCollection";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger, DialogTitle } from "@/components/ui/dialog";
 
 export default async function RiverEditor() {
   const rivers = await fetchCollection<River>("rivers");
@@ -26,6 +28,21 @@ export default async function RiverEditor() {
                   <TableCell>{river.id}</TableCell>
                   <TableCell>{river.name}</TableCell>
                   <TableCell>{river.width}</TableCell>
+                  <TableCell>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button variant="outline">查看點數據</Button>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogTitle>河流點數據</DialogTitle>
+                        <ul>
+                          {river.points.map((p, index) => (
+                            <li key={index}>{`(${p.x}, ${p.y})`}</li>
+                          ))}
+                        </ul>
+                      </DialogContent>
+                    </Dialog>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
